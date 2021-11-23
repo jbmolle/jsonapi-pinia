27e1236c28d47fc1a42b2c6350cd6dbecb023fa5
Jean-Baptiste Molle <jb.molle@vescape.com>
2021-11-23 10:35:41 +0100

    Implemented the 5 basic actions

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import type { ApiConf } from './types/apiConf'
 import { defineStore } from 'pinia'
 import { getUrl } from './utils'
+import type { ResourceObject, NewResourceObject } from './types/resourceObjects'
 
 export const useJsonApi = (resourceType: string, providedApiConf?: ApiConf) => {
   const defaultApiConf = {
@@ -30,11 +31,18 @@ export const useJsonApi = (resourceType: string, providedApiConf?: ApiConf) => {
           )
         }
         const response = await fetch(url.href, conf)
+        // If an error occured, return the response
+        if (!response.ok) {
+          return response
+        }
+        // Else add the element array to the store
         const json = await response.json()
         this.data = {
           ...this.data,
           ...json.data
         }
+        // And return the response
+        return response
       },
       async get(id: string, queryParams?: { [key: string]: any }) {
         const url = getUrl(globalApiConf.baseUrl, `/${resourceType}/${id}`)
@@ -44,22 +52,62 @@ export const useJsonApi = (resourceType: string, providedApiConf?: ApiConf) => {
             url.searchParams.append(key, queryParams[key])
           )
         }
-        await fetch(url.href, conf)
+        const response = await fetch(url.href, conf)
+        // If an error occured, return the response
+        if (!response.ok) {
+          return response
+        }
+        // The api returns one element. Add it to the store with id
+        const json = await response.json()
+        const elementData = json.data
+        this.data[id] = elementData
+        return response
       },
-      async create(body: { [key: string]: any }) {
+      async create(body: NewResourceObject) {
         const url = getUrl(globalApiConf.baseUrl, `/${resourceType}`)
         const conf = { method: 'POST', body }
-        await fetch(url.href, conf)
+        const response = await fetch(url.href, conf)
+        // If an error occured, return the response
+        if (!response.ok) {
+          return response
+        }
+        // The server can respond with a 201 Created and include the document or 204 No Content and no document
+        if (response.status === 201) {
+          const json = await response.json()
+          const elementData = json.data
+          this.data[elementData.id] = elementData
+        } else if (response.status === 204) {
+          this.data[body.id] = body
+        }
+        return response
       },
-      async update(id: string, body: { [key: string]: any }) {
+      async update(id: string, body: ResourceObject) {
         const url = getUrl(globalApiConf.baseUrl, `/${resourceType}/${id}`)
         const conf = { method: 'PATCH', body }
-        await fetch(url.href, conf)
+        const response = await fetch(url.href, conf)
+        // If an error occured, return the response
+        if (!response.ok) {
+          return response
+        }
+        // The server can respond with a 200 OK and include the document or 204 No Content and no document
+        if (response.status === 200) {
+          const json = await response.json()
+          this.data[id] = json.data
+        } else if (response.status === 204) {
+          // No document is returned so do a get request to update the document in the store
+          this.get(id)
+        }
+        return response
       },
       async delete(id: string) {
         const url = getUrl(globalApiConf.baseUrl, `/${resourceType}/${id}`)
         const conf = { method: 'DELETE' }
-        await fetch(url.href, conf)
+        const response = await fetch(url.href, conf)
+        // If the server respond with 200 or 204 then the deletion was successful
+        if (response.ok) {
+          delete this.data[id]
+        }
+        return response
       }
     }
   })()
