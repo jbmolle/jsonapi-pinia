cc0d96c8ca9385bb92b7c3a0a54caeb702b36d6f
Jean-Baptiste Molle <jb.molle@vescape.com>
2021-11-23 10:40:14 +0100

    Added fake tests

--- tests/actions.spec.ts
+++ tests/actions.spec.ts
@@ -3,8 +3,10 @@ describe('get', function () {
   beforeEach(function () {})
 
   test('should make an api call to GET an item', async () => {
+    expect(1).toBe(1)
   })
 
   test('should make an api call to GET a collection', async function () {
+    expect(1).toBe(1)
   })
 })
---
+++ tests/getters.spec.ts
@@ -0,0 +1,8 @@
+describe('get', function () {
+
+  beforeEach(function () {})
+
+  test('should get relationships', async () => {
+    expect(1).toBe(1)
+  })
+})
